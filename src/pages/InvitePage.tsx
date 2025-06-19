bb751d5110c9a94d42615add2a4ada705cda6fe1
LusGevorgyan <lusinegevorgyan197@gmail.com>
2025-06-20 01:53:15 +0400

    test

--- src/pages/InvitePage.tsx
+++ src/pages/InvitePage.tsx
@@ -2,21 +2,52 @@ import Countdown from '../components/Countdown'
 import Story from '../components/Story'
 import Program from '../components/Program'
 import Calendar from '../components/Calendar'
-
+import "../components/invite.css"
 
 export default function InvitePage() {
+  const deadline = new Date('2025-07-21T20:00:00');
+
   return (
-    <div className="page page2">
-      <section className="section countdown-section" style={{ backgroundImage: `url('/images/background2.jpg')` }}>
-        <h2>Our Wedding Countdown</h2>
-        <Countdown />
-        <p>Join us on July 20, 2025, Yerevan, Armenia</p>
+    <div className="invite-page">
+      <section className="hero">
+        <div className="overlay">
+          <h1 className="monogram">A<span>|</span>S</h1>
+          <p className="subtitle">Հարսանյաց հրավեր</p>
+          <Countdown targetDate={deadline} />
+        </div>
+      </section>
+
+      <section className="description">
+        <p>
+          Մեր կյանքի ամենագեղեցիկ օրվա մասին երազելիս միշտ պատկերացրել ենք ձեզ՝ մեր սիրելի, հարազատ ու կարևորված մարդկանց մեր կողքին։
+Ուստի մեծ սիրով հրավիրում ենք Ձեզ ներկայանալու մեր հարսանիքին՝ դարձնելով օրը առավել հիասքանչ ու հիշարժան։
+        </p>
+      </section>
+
+      <section className="love-story">
+        <div className="text-block">
+          <p className="name-top">Name</p>
+        </div>
+
+        <div className="photo-grid">
+          <div className="photo left">
+            <img src="/images/background3.jpg" alt="Couple 1" />
+            <p className="amp">&</p>
+          </div>
+          <div className="photo right-top">
+            <img src="/images/background7.jpg" alt="Couple 2" />
+          </div>
+          <div className="photo right-bottom">
+            <img src="/images/background8.jpg" alt="Couple 3" />
+            <p className="name-bottom">Name</p>
+          </div>
+        </div>
       </section>
 
         <Calendar />
-        
-      <Story />
-      <Program />
+        <Story />
+        <Program />
+
     </div>
-  )
+  );
 }
